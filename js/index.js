1702f95f22fdb83444ee09e6c4e032341e6fff25
Maria Anais Reyes <maria.anaisr@gmail.com>
2021-08-25 09:15:26 -0500

    adding checkbox to toggle axis

--- js/index.js
+++ js/index.js
@@ -1,17 +1,33 @@
-const demoContainer = document.getElementsByClassName('demo-container')[0]
+const demoContainer = document.getElementsByClassName('demo-container')[0];
+const axisElements = document.getElementsByClassName('axis')
 
 const init = () => {
-    console.log(demoContainer)
     const dropdowns = document.getElementsByClassName('dropdown');
-    const dropdownElements = Array.from(dropdowns)
+    const dropdownElements = Array.from(dropdowns);
     dropdownElements.forEach(item => {
-        item.addEventListener('change', addValue)
+        item.addEventListener('change', addValue);
     })
+    document.getElementById('axis-checkbox').addEventListener('change', toggleAxis);
 }
 
 const addValue = (e) => {
     demoContainer.style[e.target.name] = e.target.value;
-    console.log('change', e.target.value, e.target.name)
+    if(e.target.name === 'flexDirection') {
+        setAxis(e.target.value);
+    }
+    
+}
+
+const toggleAxis = (e) => {
+    Array.from(axisElements).forEach(item => {
+        item.classList.toggle('hidden');
+    })
+    demoContainer.classList.toggle('hidden');
+}
+
+const setAxis = (value) => {
+    axisElements[0].innerText = value === 'row' ? 'cross axis' : 'primary axis'
+    axisElements[1].innerText = value === 'row' ? 'primary axis' : 'cross axis'
 }
 
 init();
